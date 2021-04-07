7254b9978b08c8ba717802c55f6f2185a98412ff
Juan Steban Feijoo <steban_feijoo94@hotmail.com>
2021-04-06 23:35:14 -0500

    Parte logica ok, Falta el diseño grafico de las vistas

---
+++ ProyectoFinal/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using ProyectoFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // POST: Home/Index
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            MantenimientoUsuario mantenimiento = new MantenimientoUsuario();
+            Usuario usu = new Usuario
+            {
+                Nombre = collection["Nombre"].ToString(),
+                Edad = int.Parse(collection["Edad"].ToString()),
+                Email = collection["Email"].ToString(),
+                Telefono = int.Parse(collection["Telefono"].ToString()),
+                Ciudad = collection["Ciudad"].ToString(),
+                Interes = Boolean.Parse(collection["Interes"].ToString())
+            };
+            mantenimiento.Crear(usu);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Home/List
+        public ActionResult List()
+        {
+            MantenimientoUsuario mantenimiento = new MantenimientoUsuario();
+            return View(mantenimiento.ListarTodos());
+        }
+
+        /*
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            MantenimientoUsuario mantenimiento = new MantenimientoUsuario();
+            Usuario usu = new Usuario
+            {
+                Nombre = collection["Nombre"].ToString(),
+                Edad = int.Parse(collection["Edad"].ToString()),
+                Email = collection["Email"].ToString(),
+                Telefono = int.Parse(collection["Telefono"].ToString()),
+                Ciudad = collection["Ciudad"].ToString(),
+                Interes = Boolean.Parse(collection["Interes"].ToString())
+            };
+            mantenimiento.Crear(usu);
+            return RedirectToAction("Index");
+        }
+        */
+        
+    }
+}
--- ProyectoFinal/Models/MantenimientoUsuario.cs
+++ ProyectoFinal/Models/MantenimientoUsuario.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,63 @@ namespace ProyectoFinal.Models
         {
             string constr = ConfigurationManager.ConnectionStrings["conectBd"].ToString();
             conexion = new SqlConnection(constr);
+        }
+
+        //Metodo para la creacion de un usuario
+        public int Crear(Usuario usu)
+        {
+            Conectar();
+            SqlCommand comando = new SqlCommand(" insert into usuarios(Nombre, Edad, Email, Telefono, Ciudad, Interes) values " +
+                "(@Nombre, @Edad, @Email, @Telefono, @Ciudad, @Interes)", conexion);
+
+            comando.Parameters.Add("@Nombre", SqlDbType.VarChar);
+            comando.Parameters.Add("@Edad", SqlDbType.Int);
+            comando.Parameters.Add("@Email", SqlDbType.VarChar);
+            comando.Parameters.Add("@Telefono", SqlDbType.Int);
+            comando.Parameters.Add("@Ciudad", SqlDbType.VarChar);
+            comando.Parameters.Add("@Interes", SqlDbType.Bit);
+
+            comando.Parameters["@Nombre"].Value = usu.Nombre;
+            comando.Parameters["@Edad"].Value = usu.Edad;
+            comando.Parameters["@Email"].Value = usu.Email;
+            comando.Parameters["@Telefono"].Value = usu.Telefono;
+            comando.Parameters["@Ciudad"].Value = usu.Ciudad;
+            comando.Parameters["@Interes"].Value = usu.Interes;
+
+            conexion.Open();
+            int i = comando.ExecuteNonQuery();
+            conexion.Close();
+            return i;
+        }
+
+
+        public List<Usuario> ListarTodos()
+        {
+            Conectar();
+            List<Usuario> usuarios = new List<Usuario>();
+
+            SqlCommand com = new SqlCommand("select * from usuarios", conexion);
+
+            conexion.Open();
+            SqlDataReader registros = com.ExecuteReader();
+
+            while (registros.Read())
+            {
+                Usuario usu = new Usuario
+                {
+                    Id = int.Parse(registros["Id"].ToString()),
+                    Nombre = registros["Nombre"].ToString(),
+                    Edad = int.Parse(registros["Edad"].ToString()),
+                    Email = registros["Email"].ToString(),
+                    Telefono = int.Parse(registros["Telefono"].ToString()),
+                    Ciudad = registros["Ciudad"].ToString(),
+                    Interes = Boolean.Parse(registros["Interes"].ToString())
+                };
+
+                usuarios.Add(usu);
+            }
+            conexion.Close();
+            return usuarios;
         }
     }
 }
