a192c3d595599301059dfce5d6b57f65ed098a78
Juan Steban Feijoo <steban_feijoo94@hotmail.com>
2021-04-06 21:47:31 -0500

    creacion de clase usuario y mantenimiento usuario para los metodos de consulta en la Bd

---
+++ ProyectoFinal/Models/MantenimientoUsuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinal.Models
+{
+    public class MantenimientoUsuario
+    {
+        private SqlConnection conexion;
+
+        private void Conectar()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["conectBd"].ToString();
+            conexion = new SqlConnection(constr);
+        }
+    }
+}
---
+++ ProyectoFinal/Models/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinal.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MinLength(10, ErrorMessage = "El nombre de usuario debe tener minimo 10 caracteres")]
+        public string Nombre { get; set; }
+        [EmailAddress(ErrorMessage = "Debe ingresar un email valido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "El numero de celular es obligatorio")]
+        [MinLength(10, ErrorMessage = "El numero de celular debe tener minimo 10 caracteres")]
+        public int Telefono { get; set; }
+        [Range(15,200, ErrorMessage = "La edad debe ser un valor positivo Mayor a 15 ")]
+        public int Edad { get; set; }
+        public string Ciudad { get; set; }
+        public Boolean Interes { get; set; }
+
+    }
+}
